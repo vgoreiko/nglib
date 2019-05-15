e4de9edb1156a82aba6a08222d707c57306c8f32
vgoreiko <mbox696@gmail.com>
2019-05-15 20:30:51 +0300

    initial commit: -added angular workspace -added facade project -added library

---
+++ projects/ui-components/src/lib/ui-components.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { UiComponentsComponent } from './ui-components.component';
+
+@NgModule({
+  declarations: [UiComponentsComponent],
+  imports: [
+  ],
+  exports: [UiComponentsComponent]
+})
+export class UiComponentsModule { }
